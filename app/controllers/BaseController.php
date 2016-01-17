9f31946a74e4b1e17b8fcc8c4e75b52499d505af
John <jspears623@gmail.com>
2016-01-17 12:03:59 +0000

    bootstrap added

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -5,10 +5,22 @@ class BaseController extends \Phalcon\Mvc\Controller
 	public function initialize()
 	{
 		$this->assets
-			->collection('js')
-			->addJs('third-party/js/jquery-1.12.0.min.js');
+			 ->collection('style')
+			 ->addCss('third-party/css/bootstrap.min.css', false, false)
+			 ->addCss('css/style.css')
+			 ->setTargetPath('css/production.css')
+			 ->setTargetUrl('css/production.css')
+			 ->join(true)
+			 ->addFilter(new \Phalcon\Assets\Filters\Cssmin());
+		//takes the js or css files and combines / minifies them for faster page loads
 		$this->assets
-			->collection('css')
-			->addCss('css/style.css');
+			->collection('js')
+			->addJs('third-party/js/jquery-1.12.0.min.js', false, false)
+			->addJs('third-party/js/bootstrap.min.js', false, false)
+			->setTargetPath('js/production.css')
+			->setTargetUrl('js/production.css')
+			->join(true)
+			->addFilter(new \Phalcon\Assets\Filters\Jsmin());
+
 	}
 }	
